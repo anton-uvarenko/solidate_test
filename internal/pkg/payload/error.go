96b690ab87a168b93c4b6974cdaeb8c9e4fef9b1
anton <anthony.uvarenko@gmail.com>
2024-03-14 21:29:49 +0200

    feat(pkg error): now implements error interface and has creation function

--- internal/pkg/payload/error.go
+++ internal/pkg/payload/error.go
@@ -1,6 +1,19 @@
 package payload
 
-type ErrorResponse struct {
+import "fmt"
+
+type Error struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
+}
+
+func (e Error) Error() string {
+	return fmt.Sprintf("%d: %v", e.Code, e.Message)
+}
+
+func NewError(code int, message string) *Error {
+	return &Error{
+		Code:    code,
+		Message: message,
+	}
 }
